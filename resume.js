3b9c72e5491d61a1f4e320c7fef5ee342570ba11
arunnurak75 <arunnurak75@gmail.com>
2022-01-26 21:17:37 +0530

    resume

---
+++ resume.js
@@ -0,0 +1,27 @@
+let resume={
+        "name": "arun",
+        "email": "arun.gmail.com",
+        "phone no": "1234567890",
+        "location":{
+            "address":"1st street",
+            "pincode":"600010",
+            "city":"chennai"
+        },
+        "work":{
+            "name":"corporate",
+            "designation":"manager"
+        },
+        "education":{
+            "institution":"JCE",
+            "course":"cse"
+        },
+        "skills": {
+            "name": "Web Development",
+            "level": "Master"
+        },
+        "languages": {
+            "language": "English",
+            "fluency": "Native speaker"
+          },
+    }
+    console.log(resume);
